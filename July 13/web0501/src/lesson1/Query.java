db4842e83210c4e0374ed8d2f7bf8f9aee93358f
hongzhirui <43475731+hongzhirui@users.noreply.github.com>
2019-07-13 16:24:50 +0800

    查询数据

---
+++ July 13/web0501/src/lesson1/Query.java
@@ -0,0 +1,57 @@
+package lesson1;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+
+import my.DemoDb;
+import my.Student;
+
+/**
+ * Servlet implementation class Query
+ */
+@WebServlet("/Query")
+public class Query extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Query() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<Student> rows = DemoDb.i.list();
+		
+		//List -> JSONArray
+		JSONArray result = new JSONArray(rows);
+		
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/plain");
+		Writer writer = response.getWriter();
+		writer.write(result.toString(2));
+		writer.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
